f7328ec307dfb8c0f28fc889c2f148e8b0e1fa5e
nicemilio <nic.th@web.de>
2024-09-11 20:46:28 +0200

    Added Flowcharts

---
+++ 100 Punkte/FlowchartsAPythonStory.py
@@ -0,0 +1,11 @@
+n = input ()
+
+if (n.startswith ("Bin broke")):
+    if (n == "Bin broke, weil Schischa"):
+        print ("Wyld")
+    else:
+        print ("Digga")
+elif (len (n) == 9):
+    print ("Auf Lock")
+else:
+    print ("Zwambo")
