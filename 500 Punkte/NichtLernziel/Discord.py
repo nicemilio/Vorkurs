33c2cb516d3dd41d6557381e90b95747869a0b5b
nicemilio <nic.th@web.de>
2024-09-18 10:17:40 +0200

    Fixed token error

--- 500 Punkte/NichtLernziel/Discord.py
+++ 500 Punkte/NichtLernziel/Discord.py
@@ -10,8 +10,4 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run('MTI4NTg2OTAxMzY1MzUyMDQwNg.GfIxJ2.Yh7OK0-WajFZdRC3PN7LIAxVRiZFrhK3NFomw0')
-
-
-
-
+client.run('Token')
