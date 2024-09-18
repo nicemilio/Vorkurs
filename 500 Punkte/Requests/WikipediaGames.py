00e6cc073bd281e917759d6ad00110477c41abe2
nicemilio <nic.th@web.de>
2024-09-18 10:46:51 +0200

    Added wiki game functions

--- 500 Punkte/Requests/WikipediaGames.py
+++ 500 Punkte/Requests/WikipediaGames.py
@@ -2,14 +2,27 @@ import wikipediaapi
 
 def get_links(page):
     links = page.links
-    return enumerate(sorted(links.keys()), start=1)
+    return links
 
-wiki_wiki = wikipediaapi.Wikipedia('Wikigame', 'de')ç
+wiki_wiki = wikipediaapi.Wikipedia('Wikigame', 'de')
+page_py = wiki_wiki.page('Frühstücksfleisch')
 
-while (True):
-    page_py = wiki_wiki.page('Frühstücksfleisch')
-    links = get_links (page_py)
-    for i, link in links:
-        print (i)
-        print (link)
+while True:
+    links = get_links(page_py)
+    link_list = list(links.keys())
     
+    for i, link in enumerate(link_list, start=1):
+        print(f"Nummer {i}: {link}")
+    
+    print("In welchen Link möchtest du navigieren?")
+    selection = int(input()) - 1
+    29
+    if 0 <= selection < len(link_list):
+        selected_link = link_list[selection]
+        print(f"Navigiere zu: {selected_link}")
+        if (selected_link == "Python (Programmiersprache)"):
+            print ("Herzlichen Glückwunsch, du hast die Python Seite erreicht")
+            break
+        page_py = wiki_wiki.page(selected_link)
+    else:
+        print("Ungültige Auswahl, bitte erneut versuchen.")
