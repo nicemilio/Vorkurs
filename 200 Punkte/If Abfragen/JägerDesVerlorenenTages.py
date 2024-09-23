123ab0e8b6ef0d5e57f62df7e678436c5160892c
nicemilio <nic.th@web.de>
2024-09-23 14:02:26 +0200

    Fixed leap year

--- 200 Punkte/If Abfragen/JägerDesVerlorenenTages.py
+++ 200 Punkte/If Abfragen/JägerDesVerlorenenTages.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser("Jäger des Verlorenen Tages")
 parser.add_argument("year", help="The year you want to check for a leap-year", type=int)
 args = parser.parse_args()
 
-if (args.year % 4 == 0 or args.year % 400 == 0):
+if ((args.year % 4 == 0 and args.year % 100 != 0) or args.year % 400 == 0):
 	print ("Leap year")
 else:
 	print ("No leap year")
